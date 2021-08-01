fdf9aa71cc5ccdf5919b68fa9085c51412d00282
KD3821 <korden3821@gmail.com>
2021-08-02 00:01:08 +0300

    read_only_computed added

---
+++ Oleg_Molchanov_OOP/12_read_only_computed.py
@@ -0,0 +1,29 @@
+
+
+class Person:
+    def __init__(self, name, surname):
+        self._name = name
+        self._surname = surname
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def surname(self):
+        return self._surname
+
+    @surname.setter
+    def surname(self, value):
+        self._surname = value
+
+    @property
+    def full_name(self):
+        return f'{self._name} {self._surname}'
+
+
+p = Person('Ivan', 'Ivanov')
