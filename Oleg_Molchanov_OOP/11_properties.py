0b0a38829172d60019c02c0400ddaa08a6be6fa7
KD3821 <korden3821@gmail.com>
2021-08-05 13:18:00 +0300

    properties editted

--- Oleg_Molchanov_OOP/11_properties.py
+++ Oleg_Molchanov_OOP/11_properties.py
@@ -4,14 +4,31 @@ class Person:
     def __init__(self, name):
         self._name = name
 
-    def get_name(self):
-        print('From get_name()')
+    # def get_name(self):                               #first-second-third try
+    #     print('From get_name()')
+    #     return self._name
+    #
+    # def set_name(self, value):
+    #     print('From set_name()')
+    #     self._name = value
+
+    @property
+    def name(self):
         return self._name
 
-    def set_name(self, value):
-        print('From set_name()')
+    @name.setter
+    def name(self, value):
         self._name = value
 
-    name = property(fget=get_name, fset=set_name)
+
+    # name = property(fget=get_name, fset=set_name)     # first try
+
+    # name = property()                                 #second try
+    # name = name.getter(get_name)
+    # name = name.setter(set_name)
+
+    # name = property(get_name)                         #third try
+    # name = name.setter(set_name)
+
 
 p = Person('Dima')
